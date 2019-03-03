7fe30deee1d1a0078c7bdb340cb7dbc6ec4a55d3
danny <danixeka@gmail.com>
2019-03-03 20:07:05 +1100

    added campsite id column to amenity model and vice versa, I need to find out how to get amenity info from campsite model and display it json format

--- db/migrate/20190301031038_create_amenities.rb
+++ db/migrate/20190301031038_create_amenities.rb
@@ -2,6 +2,7 @@ class CreateAmenities < ActiveRecord::Migration[5.2]
   def change
     create_table :amenities do |t|
       t.text :name
+      t.integer :campsite_id
 
       t.timestamps
     end
